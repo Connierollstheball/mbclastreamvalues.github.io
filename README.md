b1b9362f3da538725c3cedcf799200cd1f7ed551
Connie Julie <alexnicolas155@yahoo.com>
2021-12-05 11:48:12 +0200

    Initial Commit: all necessary items
    Updates might or might not come eventually

---
+++ README.md
@@ -0,0 +1,2 @@
+# mbclavalues.github.io
+All of the credits for this site go to the original creators of 8values, this is just a remix.
---
+++ questions.js
@@ -0,0 +1,194 @@
+questions = [
+    {
+        "question": "I enjoy long and painful levels.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -5
+        }
+    },
+    {
+        "question": "I enjoy relxaing levels.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10
+        }
+    },
+    {
+        "question": "I often listen to Mask by Dream to cope while playing harder levels.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0
+        }
+    },
+    {
+        "question": "I often do not listen to any versions of Mask by Dream to cope while playing harder levels.",
+        "effect": {
+            "econ": 0,
+            "dipl": 2.5,
+            "govt": 0
+        }
+    },
+    {
+        "question": "I enjoy easy levels.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 2.5
+        }
+    },
+    {
+        "question": "I do not listen to any versions of Mask by Dream to cope while playing harder levels at all.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0
+        }
+    },
+    {
+        "question": "I would like to stream hard levels for the enjoyment of others.",
+        "effect": {
+            "econ": 5,
+            "dipl": -5,
+            "govt": -5
+        }
+    },
+    {
+        "question": "I think Connie is big brain",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0
+        }
+    },
+	{
+		"question": "Mask by Dream is the universal song of pain",
+		"effect": {
+            "econ": 5,
+            "dipl": -5,
+            "govt": 0
+        }
+	},
+	{
+		"question": "Mask by Dream is not in my playlist, not even remixes",
+		"effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0
+        }
+	},
+	{
+		"question": "Exoplanet is the universal symbol of pain",
+		"effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0
+        }
+	},
+	{
+		"question": "I enjoy playing Exoplanet",
+		"effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0
+        }
+	},
+	{
+		"question": "At least a remix of Mask by Dream is in my playlist",
+		"effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0
+        }
+	},
+    {
+        "question": "I enjoy small levels.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 2.5
+        }
+    },
+    {
+        "question": "I would listen to Mask by Dream to cope with actual pain.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0
+        }
+    },
+    {
+        "question": "I often listen to remixes of Mask by Dream to cope while playing harder levels.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0
+        }
+    },
+    {
+        "question": "I think Connie is small brain",
+        "effect": {
+            "econ": -10,
+            "dipl": 5,
+            "govt": 0
+        }
+    },
+	{
+		"question": "Mask by Dream is not in my playlist",
+		"effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0
+        }
+	},
+    {
+        "question": "I think Ice Totem by Who is a good level",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -10
+        }
+    },
+    {
+        "question": "I think Liocer by Henry is a good level",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -10
+        }
+    },
+    {
+        "question": "I think Koopshi's Skill Tower is a good level",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 5
+        }
+    },
+	{
+		"question": "I do not enjoy playing Exoplanet",
+		"effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0
+        }
+	},
+	{
+		"question": "I think Henry's levels should make it into 70-79",
+		"effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -10
+        }
+	},
+	{
+		"question": "Mask by Dream is in my playlist",
+		"effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0
+        }
+	}
+];
